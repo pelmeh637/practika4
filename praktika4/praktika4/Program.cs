db3d38542412cc856112522dd71442725afad8de
student <artem_rasskazov0406011488@mail.ru>
2020-09-25 18:52:11 +0300

    fix1

---
+++ praktika4/praktika4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+class Program
+{
+  static void Main(string[] args)
+  {
+    ArrayList myList = new ArrayListO; myList.Add("First"); myList.Add("Second");
+    myList.Add("Third");
+    myList.Add("Fourth");
+    foreach (string item in myList)
+    {
+      Console.WriteLine("Unsorted: {0}", item);
+    }
+    // Сортировка при помощи стандартного объекта сравнения 
+    myList.Sort();
+    foreach (string item in myList)
+    {
+      Console.WriteLine("   Sorted: {0}", item);
+    }
+  }
+}
+
